3b1fba9dcb76cafc6e31dcb4e0a41b84f89dea70
github cjl248 <github cjl248@cornell.edu>
2020-09-14 09:47:13 -0400

    MenuButton refactor to class component to keep state

--- src/components/MenuButton.jsx
+++ src/components/MenuButton.jsx
@@ -1,36 +1,40 @@
-import React, {useState, useEffect} from 'react'
+// import React, {useState, useEffect} from 'react'
+import React from 'react'
 import HamburgerMenu from 'react-hamburger-menu'
 
 
 //change to class component to keep track of menu status
 // OR save state in app and pass down set f and state to this component
-export default function MenuButton(props) {
+export default class MenuButton extends React.Component {
 
-  const [menu, setMenu] = useState(false)
-
-  const handleMenuClick = () => {
-    setMenu(!menu)
+  state = {
+    menuActive: false,
   }
 
-  useEffect(()=> {
-    props.toggleMenu(menu)
-  })
+  handleMenuClick = () => {
+    this.setState({
+      menuActive: !this.state.menuActive
+    }, () => {
+      this.props.toggleMenu(this.state.menuActive)
+    })
+  }
 
-  return (
-    <div className="menu-button">
-      <HamburgerMenu
-        isOpen={menu}
-	      menuClicked={handleMenuClick}
-	      width={36}
-	      height={30}
-        strokeWidth={4}
-	      color='#E2E8DD'
-	      borderRadius={5}
-	      animationDuration={0.5}>
-      </HamburgerMenu>
-    </div>
-  )
-
+  render() {
+    return (
+      <div className="menu-button">
+        <HamburgerMenu
+          isOpen={this.state.menuActive}
+  	      menuClicked={this.handleMenuClick}
+  	      width={36}
+  	      height={30}
+          strokeWidth={4}
+  	      color='#E2E8DD'
+  	      borderRadius={5}
+  	      animationDuration={0.5}>
+        </HamburgerMenu>
+      </div>
+    )
+  }
 
 
 }
