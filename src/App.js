4753d5bac73f1860a41c736f3a295eaef65b3b8e
github cjl248 <github cjl248@cornell.edu>
2020-09-13 23:16:26 -0400

    worked on basic layout and updated color scheme

--- src/App.js
+++ src/App.js
@@ -3,11 +3,23 @@ import './App.css'
 import Header from './components/Header.jsx'
 import Main from './components/Main.jsx'
 
-export default function App() {
-  return (
-    <div className="app">
-      <Header></Header>
-      <Main></Main>
-    </div>
-  )
+export default class App extends React.Component {
+
+  state = {
+    menuActive: false,
+  }
+
+  toggleMenu = (active) => {
+    console.log(active)
+  }
+
+  render() {
+    return (
+      <div className="app">
+        <Header toggleMenu={this.toggleMenu}></Header>
+        <Main menuActive={this.state.menuActive}></Main>
+      </div>
+    )
+  }
+
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,15 @@
-import React from 'react';
+import React from 'react'
+import SocialMediaContainer from '../containers/SocialMediaContainer.jsx'
+import MenuButton from './MenuButton.jsx'
+import HeaderTitle from './HeaderTitle.jsx'
 
-export default function Header() {
+export default function Header(props) {
 
   return (
     <div className="app-header">
-      <p>{`NAVIGATION`}</p>
+      <MenuButton toggleMenu={props.toggleMenu}></MenuButton>
+      <HeaderTitle></HeaderTitle>
+      <SocialMediaContainer></SocialMediaContainer>
     </div>
   )
 
