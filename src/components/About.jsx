c913fee898170274c550328357fb62d692435576
github cjl248 <github cjl248@cornell.edu>
2020-09-18 22:41:50 -0400

    created outline for different active page components: experience, resume, projects and about

---
+++ src/components/About.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Resume() {
+  return(
+    <div>
+      <span>{`ABOUT Chris Liendo`}</span>
+    </div>
+  )
+}
--- src/containers/DetailContainer.jsx
+++ src/containers/DetailContainer.jsx
@@ -1,10 +1,21 @@
 import React from 'react'
+import Resume from '../components/Resume.jsx'
+import About from '../components/About.jsx'
+import Projects from '../components/Projects.jsx'
+import Experience from '../components/Experience.jsx'
 
 export default function DetailContainer(props) {
 
+  const detailComponents = {
+    resume: <Resume></Resume>,
+    about: <About></About>,
+    projects: <Projects></Projects>,
+    experience: <Experience></Experience>
+  }
+
   return (
     <div className='detail-container'>
-      <span>{`active page: ${props.activePage.toUpperCase()}`}</span>
+      <span>{detailComponents[props.activePage]}</span>
     </div>
   )
 
