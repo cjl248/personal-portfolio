b4f88f60ec1347592b6b41ccad5659bad8e9085c
github cjl248 <github cjl248@cornell.edu>
2020-09-20 22:58:56 -0400

    basic projects page layout

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -2,8 +2,12 @@ import React from 'react'
 
 export default function Projects() {
   return(
-    <div>
-      <span>{`Projects`}</span>
+    <div className='projects-page-container'>
+      <section className='projects-header-container'>
+        <p>{`Projects`}</p>
+      </section>
+      <section className='projects-demo-container'>
+      </section>
     </div>
   )
 }
