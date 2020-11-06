3c516e2a739c6d8ca8635e5dc272267b6e2032cd
github cjl248 <github cjl248@cornell.edu>
2020-11-06 11:47:19 -0500

    Added more styling to form.

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -23,17 +23,51 @@ export default class Resume extends React.Component {
         <section className='about-main'>
           <p>{`Chris Liendo is a Fullstack Software Engineer and recent graduate of Flatiron School in  New York, NY. He holds an undergraduate degree in Information Science and Economics from Cornell University. `}</p>
           <p>{`He loves to ski and snowboard in the winter, kayak and hike in the summer and play soccer and tennis year round.`}</p>
-          <p>{`If you'd like to get in contact, you can use the form below or email him directly at cjl248 "at" cornell "dot" edu `}</p>
+          <p>{`You can find more information and download a copy of an updated Resume on the...resume page! Or if you'd like to get in contact, you can use the form below!`}</p>
         </section>
         <section className='about-form-wrapper'>
           <form className='about-form'>
-            <label className='about-label'>{`Message: `}</label>
+            <label className='contact-label'>{`CONTACT CHRIS: `}</label>
+            <span className='name-group'>
+              <label className='name-label'>{`Name: `}</label>
+              <input
+                required
+                className='name-input'
+                type='text'
+                autocomplete="chrome-off">
+              </input>
+            </span>
+            <span className='email-group'>
+              <label
+                className='email-label'
+                type='text'>
+                {`Email: `}
+              </label>
+              <input
+                className='email-input'
+                type='text'
+                autocomplete="chrome-off">
+              </input>
+            </span>
+            <span className='subject-group'>
+              <label
+                className='subject-label'
+                type='text'>
+                {`Subject: `}
+              </label>
+              <input
+                className='subject-input'
+                type='text'
+                autocomplete="chrome-off">
+              </input>
+            </span>
             <textarea
-              onChange={this.handleTyping}
               className='about-text'
-              rows='5'
-              cols='50'>
-              {this.state.text}
+              onChange={this.handleTyping}
+              value={this.state.text}
+              rows='6'
+              cols='40'
+              placeholder="message...">
             </textarea>
             <button className='about-button'>{`SUBMIT`}</button>
           </form>
